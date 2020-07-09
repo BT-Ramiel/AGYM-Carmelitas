9486b0c8dbcdc416971e2696f7bb1a383320ab4d
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-09 12:13:08 -0600

    :zap: agarciadarce - inicio y pie de página realizado

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,13 +1,68 @@
 import React,{Component} from 'react';
 import './Inicio.css';
-
+import carmelitasLogo from '../../img/logoCarmelitas.png'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css'
+import Producto from './producto/Producto'
 class Inicio extends  Component{
+    componentDidMount(){
+        var instance;
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.carousel');
+            var instances = M.Carousel.init(elems, {});
+            instance = M.Carousel.init({
+                fullWidth: true
+              });
+          });
+    }
     render() {
         return(
+            <div id="primary">
+            <div className="fondoGG center-align">
             <div className="container">
-                
+                    <img src={carmelitasLogo} className="responsive-img imagenEnMedio"/>
+                <h2 className="texto">¡Bienvenido a nuestra página!</h2>
             </div>
-
+            </div>
+            <div className="black">
+            <div className="container">
+                <div className="carousel carousel-slider">
+                    <a className="carousel-item" href="#one!"><img src="https://i.ibb.co/18VR9bs/primera-Diapo.jpg"/></a>
+                    <a className="carousel-item" href="#two!"><img src="https://i.ibb.co/QvQjXrC/segunda-Diapo.jpg"/></a>
+                    <a className="carousel-item" href="#three!"><img src="https://i.ibb.co/X2ZRm5K/3era-Diapo.jpg"/></a>
+                    <a className="carousel-item" href="#four!"><img src="https://i.ibb.co/sgQby1h/4taa-Diapo.jpg"/></a>
+                    <a className="carousel-item" href="#five!"><img src="https://i.ibb.co/d4WRC0K/5taDiapo.jpg"/></a>
+                    <a className="carousel-item" href="#six!"><img src="https://i.ibb.co/z6hgX1n/6taDiapo.jpg"/></a>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/4dDxMWL/piepiepie.jpg" className="responsive-img"/>
+                </div>                
+            </div>
+            </div>
+            <div className="cafecito">
+            <div className="container">
+            <div className="row">
+                <div className="col s12">
+                    <h4 className="orange-text text-lighten">¡Conozca nuestros productos!</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className ="col s6">
+                    <Producto urlImagen="https://i.ibb.co/RzcZ83T/prod1.jpg" titulo="Café Gourmet molido 1 libra" precio ="C$ 150.00" />
+                </div>
+                <div className ="col s6">
+                    <Producto urlImagen="https://i.ibb.co/b1PkxGD/prod2.jpg" titulo="Rosquillas Mixtas Alonda" precio ="C$ 25.00" />
+                </div>
+                <div className ="col s6">
+                    <Producto urlImagen="https://i.ibb.co/nkM64ph/prod3.jpg" titulo="Panini Pavo" precio ="C$ 85.00" />
+                </div>
+                <div className ="col s6">
+                    <Producto urlImagen="https://i.ibb.co/JFsxYYJ/prod4.jpg" titulo="Green Cool" precio ="C$ 50.00" />
+                </div>
+            </div>
+            </div>
+            </div>
+            </div>
         )
     };
 }
--- src/components/pie_pagina/PieDePagina.js
+++ src/components/pie_pagina/PieDePagina.js
@@ -6,20 +6,20 @@ class PieDePagina extends Component{
     render() {
         return(
           <div>
-        <footer class="page-footer">
+        <footer className="page-footer">
           <div className="tituloConImagen">
-          <div class="container">
-            <div class="row">
-              <div class="col s12">
+          <div className="container">
+            <div className="row">
+              <div className="col s12">
                 <h4>Contactos</h4>
               </div>
-              <div class="col s12">
+              <div className="col s12">
                 <h5>Celular: <a href="tel:81380937">+505 81380937</a></h5>
               </div>
-              <div class="col s12">
+              <div className="col s12">
                 <h5>Redes Sociales</h5>
               </div>
-              <div class="col s4"> 
+              <div className="col s4"> 
               <a href="facebook.com"><div className="logoFb">Síguenos en facebook!</div> </a>
               </div>
               <div className="col s4">
@@ -29,13 +29,13 @@ class PieDePagina extends Component{
               <a href="instagram.com"><div className="logoTw">Síguenos en Twitter!</div></a>
               </div>
             </div>
-            <div class ="col s12">
+            <div className="col s12">
             <h4>Correo: <a href="mailto::agarciadare@gmail.com">agarciadarce@gmail.com</a></h4>
             </div>
           </div>
           </div>
-          <div class="footer-copyright">
-            <div class="container center-align">
+          <div className="footer-copyright">
+            <div className="container center-align">
             <img src={logoCarmelitas} width="150px"className="logoCC"/>
             </div>
           </div>
