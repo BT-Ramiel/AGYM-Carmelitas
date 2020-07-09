0fbf4dc4731fc316a9690558755e280b38b72e19
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-08 19:18:24 -0600

    :boom: agarciadarce creacion de repo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navegacion from "./components/navBar/Navegacion";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,7 +11,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ReactDOM.render(
+    <React.StrictMode>
+        <Navegacion/>
+    </React.StrictMode>,
+    document.getElementById('navegacion')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
