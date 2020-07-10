8e9a6ea2728223c0e98bacacad047837f94bab8d
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-10 14:16:19 -0600

    Updates

---
+++ precache-manifest.2a5f5b6eda1f615b0edd2e443e94b63b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7a38c7157f4990133d5a560232942b",
+    "url": "/AGYM-Carmelitas/index.html"
+  },
+  {
+    "revision": "64bdf78f91b72d1d28b0",
+    "url": "/AGYM-Carmelitas/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "31d057c008dc34546142",
+    "url": "/AGYM-Carmelitas/static/css/main.fc94c6a4.chunk.css"
+  },
+  {
+    "revision": "64bdf78f91b72d1d28b0",
+    "url": "/AGYM-Carmelitas/static/js/2.20012692.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/AGYM-Carmelitas/static/js/2.20012692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d057c008dc34546142",
+    "url": "/AGYM-Carmelitas/static/js/main.a22160e4.chunk.js"
+  },
+  {
+    "revision": "2d664707202a83ad494f",
+    "url": "/AGYM-Carmelitas/static/js/runtime-main.01890a5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AGYM-Carmelitas/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f06d27bebfc788d57dc7ea21dc69bbd1",
+    "url": "/AGYM-Carmelitas/static/media/logoCarmelitas.f06d27be.png"
+  }
+]);
