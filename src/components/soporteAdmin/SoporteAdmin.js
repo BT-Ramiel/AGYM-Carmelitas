f23e775c917bc665416427af1cd7e888a5f6bf87
Xenofixion <davila.bojorquez@gmail.com>
2020-07-10 12:45:22 -0600

    Xenofixion - soporteAdmin terminado

--- src/components/soporteAdmin/SoporteAdmin.js
+++ src/components/soporteAdmin/SoporteAdmin.js
@@ -4,8 +4,24 @@ import './SoporteAdmin.css'
 class SoporteAdmin extends Component{
 
     render(){
+        const altura = window.innerHeight - 64;
         return(
-            ''
+            <div className='soporteAdminContainer' style={{height:altura +'px'}}>
+                <p id="titulo"><i class="medium material-icons">mail</i> ¿Hay algún error en el sistema? Envíanos un mensaje</p>
+                <div class="input-field col s6">
+                    <input id="asunto" type="text" class="validate"></input>
+                    <label for="asunto">Asunto</label>
+                </div>
+                <div id='mensajeCampo' class="input-field col s12">
+                    <textarea id="textarea1" class="materialize-textarea" style={{height: 200+'px', 'max-height':200+'px'}}></textarea>
+                    <label for="textarea1">Mensaje</label>
+                </div>
+                <div id='enviarMensaje'>
+                    <p>
+                    Enviar Mensaje
+                    </p>
+                </div>
+            </div>
         )
     }
 }
